82c3d8b0e956413a25c7e32698a8dae6c5566e9e
0seo8 <0seo23@gmail.com>
2022-05-07 16:24:56 +0900

    feat: 플러그인

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import {createApp} from 'vue'
 import App from './App'
 import router from './routes/index.js'
-import store from './store/index.js'
+import store from './plugins/loadImage'
+import loadImge from './store/index.js'
 
 createApp(App)
   .use(router)
   .use(store)
+  .use(loadImge)
   .mount('#app')
