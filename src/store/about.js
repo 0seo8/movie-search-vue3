e18db9f2d08f0d8a293646ca40d7fe87efe2c354
0seo8 <0seo23@gmail.com>
2022-05-08 18:58:01 +0900

    fix: 로고수정

--- src/store/about.js
+++ src/store/about.js
@@ -5,7 +5,7 @@ export default {
     email: '0seo8@naver.com',
     blog: 'https://velog.io/@0seo8',
     phone: '+82-10-1234-5678',
-    image: '/src/assets/0seo.jpg'
+    image: './src/assets/0seo.jpg'
   })
   
 }
