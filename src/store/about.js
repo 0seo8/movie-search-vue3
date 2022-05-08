36f59d6dfdd6e23b553bfe5821ed449704ae0c1b
0seo8 <0seo23@gmail.com>
2022-05-08 21:57:55 +0900

    fix:이미지 경로 수정

--- src/store/about.js
+++ src/store/about.js
@@ -5,7 +5,7 @@ export default {
     email: '0seo8@naver.com',
     blog: 'https://velog.io/@0seo8',
     phone: '+82-10-1234-5678',
-    image: '/src/assets/0seo.png'
+    image: 'https://postfiles.pstatic.net/MjAyMjA1MDhfMjg1/MDAxNjUyMDE0NjA2MDQ3.0HMi3UdPmsnJ0iadigLjvZEK0MwSLWOINEwd1HTl39kg.cyegBEad3ICUT-JlMF3UTq4I4QjWfvok2WabTAQpayog.PNG.0seo8/toonmecom_901ad5.png?type=w966'
   })
   
 }
