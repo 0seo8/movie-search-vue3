37087e4d0b604de2059398c9f79ebc3a77e5d8c8
0seo8 <0seo23@gmail.com>
2022-05-08 21:39:19 +0900

    feat: gitignore

--- src/store/movie.js
+++ src/store/movie.js
@@ -4,63 +4,83 @@ import _uniqBy from 'lodash/uniqBy'
 const _defaultMessage = 'Search for the movie title!'
 
 export default {
+  // 현재 파일(movie.js)을 Store 모듈로 활용하려면 다음 옵션이 필요합니다.
   namespaced: true,
+
+  // Vue.js data 옵션과 유사합니다.
+  // 상태(State)는 함수로 만들어서 객체 데이터를 반환해야 가변 이슈(데이터 불변성)가 발생하지 않습니다!
   state: () => ({
     movies: [],
     message: _defaultMessage,
     loading: false,
+    theMovie: {}
   }),
+
+  // Vue.js computed 옵션과 유사합니다.
   getters: {},
+
+  // Vue.js methods 옵션과 유사합니다.
+  // 상태(State)는 변이(Mutations)를 통해서만 값을 바꿀 수 있습니다.
   mutations: {
-    updateState(state, payload) { 
+    updateState(state, payload) {
       Object.keys(payload).forEach(key => {
         state[key] = payload[key]
       })
     },
     resetMovies(state) {
-      state.movies = [],
-      state.message=_defaultMessage
+      state.movies = []
+      state.message = _defaultMessage
       state.loading = false
     }
   },
+
+  // Vue.js methods 옵션과 유사합니다.
+  // 변이(Mutations)가 아닌 나머지 모든 로직을 관리합니다.
+  // 비동기로 동작합니다.
   actions: {
-    async searchMovies({state, commit}, payload) {
-      if(state.loading) return
+    async searchMovies({ state, commit }, payload) {
+      // const { title, type, number, year } = payload
+      if (state.loading) return
+
       commit('updateState', {
         message: '',
-        loading: true,
-        theMovie: {}
+        loading: true
       })
 
       try {
         const res = await _fetchMovie({
           ...payload,
-          page:1
+          page: 1
         })
         const { Search, totalResults } = res.data
         commit('updateState', {
           movies: _uniqBy(Search, 'imdbID')
         })
-  
-        const total = parseInt(totalResults, 10) //숫자로변환
-        const pageLength = Math.ceil(total / 10) //10개씩 가져오면 총 몇개의 페이지를 가져와야하는지 체크
-  
-        if(pageLength > 1) {  //pageLength가 1이라면 한개의 페이지로 모든 정보를 가지고 온것을 유추할 수 있다. 따라서 그렇지 않은 경우
-          for(let page = 2; page <=pageLength; page++) {
-            if(page > (payload.number /10)) break
+
+        // ceil = 올림!
+        const total = parseInt(totalResults, 10)
+        const pageLength = Math.ceil(total / 10) // 총 페이지의 길이
+
+        // 추가 요청!
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1) {
+            if (page > (payload.number / 10)) break
             const res = await _fetchMovie({
               ...payload,
               page
             })
-            const {Search} = res.data
+            const { Search } = res.data
             commit('updateState', {
-              movies: [...state.movies, ..._uniqBy(Search, 'imdbID')] //기존데이터 + 새로가져온 데이터
+              movies: [
+                ...state.movies,
+                ..._uniqBy(Search, 'imdbID')
+              ]
             })
           }
         }
-      }catch({message}){
+      } catch ({ message }) {
         commit('updateState', {
-          movies: [], //초기화
+          movies: [],
           message
         })
       } finally {
@@ -69,15 +89,17 @@ export default {
         })
       }
     },
-    async searchMovieWithId({state, commit}, payload) {
-      if(state.loading) return
+    async searchMovieWithId({ state, commit }, payload) {
+      // const { id } = payload
+      if (state.loading) return
+
       commit('updateState', {
         theMovie: {},
         loading: true,
       })
+
       try {
         const res = await _fetchMovie(payload)
-        console.log(res.data)
         commit('updateState', {
           theMovie: res.data
         })
@@ -85,7 +107,7 @@ export default {
         commit('updateState', {
           theMovie: {}
         })
-      }finally {
+      } finally {
         commit('updateState', {
           loading: false
         })
@@ -94,6 +116,7 @@ export default {
   }
 }
 
+// eslint-disable-next-line
 async function _fetchMovie(payload) {
   return await axios.post('/.netlify/functions/movie', payload)
 }
