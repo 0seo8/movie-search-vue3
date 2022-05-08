a378f4442c1d954835daa2e4076429921ce58e12
0seo8 <0seo23@gmail.com>
2022-05-08 19:22:55 +0900

    feat: netlify-functions

---
+++ functions/hello.js
@@ -0,0 +1,10 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode:200,
+    body: JSON.stringify({
+      name: '0seo8',
+      age: 93,
+      email: '0seo8@naver.com'
+    })
+  }
+}
